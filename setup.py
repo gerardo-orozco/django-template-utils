87563fd7cdc1b0d1eff25bf752ce56398305597d
Gerard Orozco <orozcomgera@gmail.com>
2012-12-06 14:38:15 -0600

    Fixed path to README

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup
 
-README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 
 setup(
     name='django-template-utils',
