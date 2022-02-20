650e0004d7d6ba02a0eb3d09aa3e5fdefb115593
Marco A Rojas <marquicus@gmail.com>
2022-02-19 23:35:37 -0600

    [FIX] permission

--- template_utils/templatetags/templateutils_filters.py
+++ template_utils/templatetags/templateutils_filters.py
@@ -5,6 +5,7 @@ import datetime
 from django.utils import timezone
 from django import template
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import Permission
 from django.template.defaultfilters import stringfilter
 from django.forms import (
     CharField,
